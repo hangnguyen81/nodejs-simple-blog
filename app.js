92ebcc0ddaa78a5b8dc669adfc28ac05ef8d97d7
hangnguyen81 <hangntt@gmail.com>
2021-11-24 10:27:47 +0200

    Delete function is completed

--- app.js
+++ app.js
@@ -60,6 +60,15 @@ app.get('/blogs/:id', (req, res)=>{
         .catch(err => console.log(err));
 })
 
+app.delete('/blogs/:id',(req, res) =>{
+    const id = req.params.id;
+    Blog.findByIdAndDelete(id)
+        .then(result => {
+            res.json({redirect: '/blogs'})
+        })
+        .catch(err => console.log(err));
+})
+
 app.use((req, res) =>{
     res.status(404).render('404', {title:'Not found'});
 })
